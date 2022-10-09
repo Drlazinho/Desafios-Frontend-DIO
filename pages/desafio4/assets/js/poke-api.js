1139d165b8b0469d5dd2d258ba2118d9b0be11bb
Drlazinho <lazbonfim@hotmail.com>
2022-10-08 21:31:59 -0300

    desafio4: req pokeapi

---
+++ pages/desafio4/assets/js/poke-api.js
@@ -0,0 +1,12 @@
+const pokeApi = {}
+
+pokeApi.getPokemons = (offset = 0, limit = 20) => {
+  const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+  return fetch(url)
+  .then((res) => res.json())
+  .then((jsonBody) => jsonBody.results)
+  .catch(error => {
+    console.log(error)
+  })
+}
